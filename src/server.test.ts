5adc002085e1d5e6afa0627297874d4bf79a888a
Matt Kantor <the.matt.kantor@gmail.com>
2025-10-14 09:31:03 -0400

    Make server tests robust to port collisions

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,8 +1,39 @@
 import assert from 'node:assert'
+import net from 'node:net'
 import test, { suite } from 'node:test'
 import { createServer } from './server.js'
 
-const randomPort = Math.floor(Math.random() * (65535 - 1024)) + 1024
+const getArbitraryPort = () => Math.floor(Math.random() * (65535 - 1024)) + 1024
+
+const isPortTaken = (port: number) =>
+  new Promise<boolean>((resolve, reject) => {
+    const probe = net
+      .createServer()
+      .once('listening', () =>
+        probe.once('close', () => resolve(false)).close(),
+      )
+      .once('error', error =>
+        'code' in error && error.code == 'EADDRINUSE'
+          ? resolve(true)
+          : reject(error),
+      )
+      .listen(port)
+  })
+
+let availablePort: number | undefined = undefined
+let remainingAttempts = 10 // Try up to this many ports before giving up.
+while (remainingAttempts > 0) {
+  remainingAttempts = remainingAttempts - 1
+  const arbitraryPort = getArbitraryPort()
+  const portIsTaken = await isPortTaken(arbitraryPort)
+  if (!portIsTaken) {
+    availablePort = arbitraryPort
+    break
+  }
+}
+if (availablePort === undefined) {
+  throw new Error('Could not find an available port')
+}
 
 suite('server', _ => {
   test('lifecycle', async _ => {
@@ -11,7 +42,7 @@ suite('server', _ => {
       errorPage: 'error.js',
       pageFilenameSuffix: '__page.js',
     })
-    assert.deepEqual(await server.listen(randomPort), undefined)
+    assert.deepEqual(await server.listen(availablePort), undefined)
     assert.deepEqual(await server.close(), undefined)
   })
 
@@ -19,7 +50,7 @@ suite('server', _ => {
     const server = createServer({
       publicDirectory: '/dev/null',
     })
-    assert.deepEqual(await server.listen(randomPort), undefined)
+    assert.deepEqual(await server.listen(availablePort), undefined)
     assert.deepEqual(await server.close(), undefined)
   })
 })
