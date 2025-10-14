ca6cd5b4ccca5904e0017f166e0fc3e25c6a54bc
Matt Kantor <the.matt.kantor@gmail.com>
2025-10-14 07:51:15 -0400

    Ditch use case, make it a library
    - Remove content and other use-case-specific stuff.
    - Rename the package.
    - Export public API from `index.ts`.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import { createServer } from './server.js'
-
-const port = 80
-
-const server = createServer({
-  publicDirectory: `${import.meta.dirname}/content`,
-})
-server.listen(port)
-console.log(`Server is listening on port ${port}…`)
+export * from './page.js'
+export * from './server.js'
