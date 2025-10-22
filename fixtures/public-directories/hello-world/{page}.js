a1788ddc655a0c83383a02ac09e219528d38d05b
Matt Kantor <the.matt.kantor@gmail.com>
2025-10-22 13:45:33 -0400

    Test request handling

---
+++ fixtures/public-directories/hello-world/{page}.js
@@ -0,0 +1,16 @@
+import { createElement } from '@superhighway/silk'
+import { page } from '../../../dist-test/page.js'
+
+export default page(request =>
+  createElement(
+    'html',
+    { lang: 'en' },
+    createElement(
+      'head',
+      null,
+      createElement('title', null, 'Greeting'),
+      createElement('link', { rel: 'stylesheet', href: 'style.css' }),
+    ),
+    createElement('body', null, 'Hello, world!'),
+  ),
+)
